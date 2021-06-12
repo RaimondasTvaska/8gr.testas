bae7808a63f62290d45815397a2f749a511c8571
RaimondasTvaska <rtvaska@gmail.com>
2021-06-12 10:24:38 +0300

    Nieko gero

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,43 @@ Visas funkcijas iškvieskite vieną po kitos, žemiau šio komentaro.
 Taip pat parašykite funkciją kuri: isikviesdama save pačią atspausdina skaičius nuo 1 iki 10
 
 */
+countKavine();
+
+
+function countKavine() {
+    kavine = [];
+    for (let i = 0; i < kavine.length; i++) {        
+    }
+    console.log(data);
+}
+
+function filterKaunas() {
+    let result = data.filter((adresas)=>adresas.type === "Kaunas");
+console.log(result);
+}
+
+
+
+function countFirstKavineDish() {
+    
+}
+
+function countKaunasDifferent(params) {
+    
+}
+
+function countAverage(params) {
+    
+}
+
+function comparisonAverage(params) {
+    
+}
+
+function countVegetarianDishes(params) {
+    
+}
+
+function comparisonVlnVegetarian(params) {
+    
+}
